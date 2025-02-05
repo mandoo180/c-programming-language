9a664761803c28c4da8929a554ed7d553ac615df
Kyeong Soo Choi <mandoo180@gmail.com>
2025-02-05 18:05:59 +0900

    first commit

---
+++ code/4.1/compile.ps1
@@ -0,0 +1,2 @@
+gcc .\grep.c ..\utils\getline.c
+Get-Content .\text.txt | .\a.exe
---
+++ code/4.1/grep.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+
+int strindex(char source[], char searchfor[]);
+int getline(char line[], int max);
+
+char pattern[] = "ould";
+
+int main() {
+  char line[MAXLINE];
+  int found = 0;
+  while (getline(line, MAXLINE) > 0) {
+    if (strindex(line, pattern) >= 0) {
+      printf("%s", line);
+      found++;
+    }
+  }
+  return found;
+}
+
+int strindex(char s[], char t[]) {
+  int i, j , k;
+  for (i = 0; s[i] != '\0'; i++) {
+    for (j = i, k = 0; t[k] != '\0' && s[j] == t[k]; j++, k++) {
+      
+    }
+    if (k > 0 && t[k] == '\0') {
+      return i;
+    }
+  }
+  return -1;
+}
